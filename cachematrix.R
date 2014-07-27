089c13f2fdbbe955f0712703f4ba3d8e44d54120
sapper9 <jf9k@outlook.com>
2014-07-26 20:30:16 -0400

    Commit before comments
    Function build complete, ending comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,21 @@
 ##This is a test comment
 makeCacheMatrix <- function(x = matrix()) {
 
+    I <-NULL
+    set<-function (y) {
+        
+        x<<-y
+        I<<-NULL
+        
+    }
+    
+    get <-function() x
+    setinverse<-function(inverse) I<<-inverse
+    getinverse<-function() I
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
 
@@ -12,4 +27,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    I<-x$getinverse()
+    if(!is.null(I)) {
+        print("Got Cached data")
+        return(I)
+    }
+    data<-x$get()
+    
+    I<-function(data){
+        if (det(data)!=0) return(solve(data))
+        else
+            return(print("The matrix is singular"))
+    }
+    x$setinverse(I) 
+    I
+        
 }
+
